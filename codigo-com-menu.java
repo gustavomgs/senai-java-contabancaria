abb3be469be48cc2f451ceca504d6b87f3ab5114
gustavomgs <53432968+gustavomgs@users.noreply.github.com>
2023-05-13 09:04:10 -0300

    Create codigo-com-menu.java

---
+++ codigo-com-menu.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+class ContaBancaria {
+    
+  static double saldo;  
+    
+    
+  public static double depositar(double valor) {
+      
+      saldo = saldo + valor;
+        
+    return saldo;
+  }
+  
+  public static double sacar(double valor) {
+      
+      saldo = saldo + valor;
+        
+    return saldo;
+  } 
+  
+  public static double exibirSaldo() {
+      
+    System.out.print(saldo);
+        
+    return saldo;
+  }
+  
+  public static void menu(){
+    Scanner teclado = new Scanner(System.in);   
+
+    int opcao;
+    
+    System.out.print("Menu do meu sistema\n\n");
+    System.out.print("------------------------");
+    System.out.print("Por favor selecione uma opcao:\n");
+    System.out.print("------------------------\n\n");
+    System.out.print("[1] - Depositar\n");
+    System.out.print("[2] - Sacar\n");
+    System.out.print("[3] - Exibir saldo\n");
+    System.out.print("[4] - Sair do sistema\n");
+    
+    opcao = teclado.nextInt();       
+    
+    switch (opcao) {
+      case 1:
+      depositar(10.0);
+      break;
+
+      case 2:
+      sacar(10.0);
+      break;
+
+      case 3:
+      exibirSaldo();
+      break;
+
+      case 4:
+      System.exit(0);
+      break;
+
+    default:
+      System.out.printf("Insira uma opção válida");
+      main(null);
+    }
+  }
+  
+
+	public static void main(String args[]){  
+   
+   saldo = 0;
+
+    menu();    
+    
+    System.out.print(saldo);
+    
+    System.exit(0);
+		
+	}
+
+}
